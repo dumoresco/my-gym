c926dd520838560c672ba121bcad1d1d71654b9c
dumoresco <eduardomorescoiost@gmail.com>
2025-03-19 20:37:58 -0300

    fx

--- web/src/components/nav-user.tsx
+++ web/src/components/nav-user.tsx
@@ -34,8 +34,8 @@ export function NavUser({ user }: { user: User }) {
             >
               <DynamicIconLogo width={32} height={32} alt={"Logo"} />
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user?.name}</span>
+                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
